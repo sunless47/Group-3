946138a3a94648ac0cc576307979de494f7bb657
Lizz064 <eliza.mijide@gmail.com>
2024-06-27 21:20:39 +0300

    Created a backend and a frontend

---
+++ Backend/src/test/java/Group/three/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Group.three.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
