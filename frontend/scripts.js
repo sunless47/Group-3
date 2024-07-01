4f9a48f9d196b5fc74476e0c4a52f40bea01f1ce
sunless13 <sunlesskelv@gmail.com>
2024-07-01 20:40:35 +0300

    feat: Functional
    can operate crud operations

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,24 +1,183 @@
-/********** Calculator with JS ***********/
+/********** Get on init ***********/
+// using fetch api request method
+fetch("http://localhost:8080/api/users")
+  .then((response) => {
+    // test if response is ok
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Network Response Error");
+    }
+  })
+  .then((data) => {
+    // es6 for of loop to get all data
+    for (let dat of data) {
+      //console.log(dat);
+      let dbData = document.createElement("li");
+      dbData.innerText = `${dat.id} ${dat.firstName} ${dat.lastName} ${dat.email}`;
+      document.getElementById("getReq").appendChild(dbData);
+    }
+  })
+  // handle errors
+  .catch((error) => {
+    console.error("Error: ", error);
+  });
+
+/********** Get ***********/
+// get data from api
+function apiGet() {
+  // rm outdated list
+  let list = document.getElementById("getReq");
+  list.innerHTML = "";
+
+  // using fetch api request method
+  fetch("http://localhost:8080/api/users")
+    .then((response) => {
+      // test if response is ok
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Network Response Error");
+      }
+    })
+    .then((data) => {
+      // es6 for of loop to get all data
+      for (let dat of data) {
+        //console.log(dat);
+        let dbData = document.createElement("li");
+        dbData.innerText = `${dat.id} ${dat.firstName} ${dat.lastName} ${dat.email}`;
+        document.getElementById("getReq").appendChild(dbData);
+      }
+    })
+    // handle errors
+    .catch((error) => {
+      console.error("Error: ", error);
+    });
+}
+
+/********** Post ***********/
+// add new user
 function apiPost() {
   const firstName = document.getElementById("apiFN").value;
   const lastName = document.getElementById("apiLN").value;
   const email = document.getElementById("email").value;
 
-  // transform data to object in JSON format
-  const apiReq = new Object();
-  apiReq.firstName = firstName;
-  apiReq.lastName = lastName;
-  apiReq.email = email;
+  // API endpoint for creating a new user
+  const apiUrl = "http://localhost:8080/api/users";
 
-  const apiJsonString = JSON.stringify(apiReq);
+  // Form data to be sent in the request body
+  const formData = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+  };
 
-  // checking the payload
-  console.log(apiJsonString);
+  // Make a POST request using the Fetch API
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Network Response Error");
+      }
+    })
+    .then((newData) => {
+      // Process the newly created user data
+      console.log("New User Data:", newData);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
 
-  // sending payload to the api
-  const xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "http://localhost:8080/api/users");
-  xhttp.setRequestHeader("Content-type", "application/json");
-  //xhttp.setRequestHeader("Access-Control-Allow-Headers", "*");
-  xhttp.send(apiJsonString);
+/********** Put ***********/
+// change user
+function apiPut() {
+  const idNum = document.getElementById("idNum").value;
+  const firstName = document.getElementById("apiFN").value;
+  const lastName = document.getElementById("apiLN").value;
+  const email = document.getElementById("email").value;
+
+  // API endpoint for creating a new user
+  const apiUrl = `http://localhost:8080/api/users/${idNum}`;
+
+  // Form data to be sent in the request body
+  const formData = {
+    idNum: idNum,
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+  };
+
+  // Make a put request using the Fetch API
+  fetch(apiUrl, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Network Response Error");
+      }
+    })
+    .then((newData) => {
+      // Process the newly created user data
+      console.log("Modified:", newData);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+/********** delete ***********/
+// add new user
+function apiDelete() {
+  const idNum = document.getElementById("idNum").value;
+  const firstName = document.getElementById("apiFN").value;
+  const lastName = document.getElementById("apiLN").value;
+  const email = document.getElementById("email").value;
+
+  // API endpoint for creating a new user
+  const apiUrl = `http://localhost:8080/api/users/${idNum}`;
+
+  // Form data to be sent in the request body
+  const formData = {
+    idNum: idNum,
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+  };
+
+  // Make a delete request using the Fetch API
+  fetch(apiUrl, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Network Response Error");
+      }
+    })
+    .then((newData) => {
+      // Process the newly created user data
+      console.log("Deleted:", newData);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
